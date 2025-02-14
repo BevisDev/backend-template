4f299d0b4b4c85d9f8b41f5c1fa43a517db598a1
BINHTT <truongthanh.binh@vietcredit.com.vn>
2025-02-14 10:40:55 +0700

    migration helper

--- go-backend/migration/migration.go
+++ go-backend/migration/migration.go
@@ -1,16 +1,23 @@
 package migration
 
 import (
+	"context"
+	"errors"
+	"os"
+
 	"github.com/BevisDev/backend-template/consts"
+	"github.com/BevisDev/backend-template/utils"
 	"github.com/jmoiron/sqlx"
 	"github.com/pressly/goose/v3"
-	"os"
 )
 
 type Migration struct {
-	Dir     string
-	sqlx    *sqlx.DB
-	TypeSQL string
+	Dir        string
+	Sqlx       *sqlx.DB
+	TypeSQL    string
+	TimeoutSec int
+	Ctx        context.Context
+	Version    int64
 }
 
 func (m *Migration) Init() error {
@@ -22,6 +29,8 @@ func (m *Migration) Init() error {
 	case consts.Postgres:
 		dialect = "postgres"
 		break
+	default:
+		return errors.New("type SQL unsupported")
 	}
 	if err := goose.SetDialect(dialect); err != nil {
 		return err
@@ -29,17 +38,52 @@ func (m *Migration) Init() error {
 	if _, err := os.Stat(m.Dir); os.IsNotExist(err) {
 		return err
 	}
+	goose.SetTableName("db_version")
+	if m.Ctx == nil {
+		m.Ctx = context.Background()
+	}
 	return nil
 }
 
-func (m *Migration) RunMigration() error {
+func (m *Migration) Up() error {
 	if err := m.Init(); err != nil {
 		return err
 	}
-	if err := goose.Up(m.sqlx.DB, m.Dir); err != nil {
+	ctx, cancel := utils.CreateCtxTimeout(m.Ctx, m.TimeoutSec)
+	defer cancel()
+	if m.Version != 0 {
+		if err := goose.UpToContext(ctx, m.Sqlx.DB, m.Dir, m.Version); err != nil {
+			return err
+		}
+	} else {
+		if err := goose.UpContext(ctx, m.Sqlx.DB, m.Dir); err != nil {
+			return err
+		}
+	}
+
+	if err := goose.Status(m.Sqlx.DB, m.Dir); err != nil {
 		return err
 	}
-	if err := goose.Status(m.sqlx.DB, m.Dir); err != nil {
+	return nil
+}
+
+func (m *Migration) Down() error {
+	if err := m.Init(); err != nil {
+		return err
+	}
+	ctx, cancel := utils.CreateCtxTimeout(m.Ctx, m.TimeoutSec)
+	defer cancel()
+	if m.Version != 0 {
+		if err := goose.DownToContext(ctx, m.Sqlx.DB, m.Dir, m.Version); err != nil {
+			return err
+		}
+	} else {
+		if err := goose.DownContext(ctx, m.Sqlx.DB, m.Dir); err != nil {
+			return err
+		}
+	}
+
+	if err := goose.Status(m.Sqlx.DB, m.Dir); err != nil {
 		return err
 	}
 	return nil
